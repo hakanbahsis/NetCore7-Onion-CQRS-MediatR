d74de663dabb4cd931c85c7f3053454cc95c912b
hakanbahsis <hakan.bahsis@hotmail.com>
2024-01-14 01:39:56 +0300

    CreatedEntities

---
+++ Core/Api.Domain/Common/Brand.cs
@@ -0,0 +1,16 @@
+﻿using Api.Domain.Common.Base;
+
+namespace Api.Domain.Common;
+public class Brand : EntityBase
+{
+    public Brand()
+    {
+        
+    }
+    public Brand(string name)
+    {
+        Name=name;
+    }
+    public required string Name { get; set; }
+}
+
